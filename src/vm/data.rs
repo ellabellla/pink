f23d10f93dbd310052d64b3bccbaa9c360119b0c
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-07-19 10:36:06 +1000

    data num to ref, + ret pntr to frame, opt with err

--- src/vm/data.rs
+++ src/vm/data.rs
@@ -1,32 +1,26 @@
 use std::{str::Chars, iter::Peekable};
 
-use super::{InstrError, parse_number};
+use super::{InstrError, parse_number, Reference, VM};
 
 #[allow(dead_code)]
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum Data {
-    Number(f64),
-    Frame(usize, usize),
+    Reference(Reference),
+    Frame(usize, usize, usize),
 }
 
 #[allow(dead_code)]
 impl Data {
-    pub fn to_number(&self) -> Option<f64>{
+    pub fn to_number(&self, vm: &mut VM) -> Option<f64>{
         match self {
-            Data::Number(num) => Some(*num),
-            Data::Frame(_, _) => None,
+            Data::Reference(reference) => reference.to_number(0, 0, vm),
+            Data::Frame(_, _, _) => None,
         }
     }
 }
 
-impl Default for Data {
-    fn default() -> Self {
-        Data::Number(0.0)
-    }
-}
-
+#[allow(dead_code)]
 impl Data {
-
     pub fn from_str(chars: &mut Peekable<Chars>) -> Result<Self, InstrError> {
         if let Some(c) = chars.peek() {
             if *c == 'F' {
@@ -35,25 +29,24 @@ impl Data {
                 if let Some(c) = chars.next() {
                     if c == ',' {
                         let index2 = parse_number(chars)?;
-                        return Ok(Data::Frame(index.floor() as usize, index2.floor() as usize))
+                        return Ok(Data::Frame(index.floor() as usize, 0, index2.floor() as usize))
                     }
                 }
 
             } else {
-                let index = parse_number(chars)?;
-                return Ok(Data::Number(index))
+                return Reference::from_str(chars).and_then(|reference| Ok(Data::Reference(reference)))
             }
         } 
 
-        Err(InstrError::new("couldn't parse data".to_string()))
+        Err(InstrError::new("couldn't parse data"))
     }
 }
 
 impl ToString for Data {
     fn to_string(&self) -> String {
         match self {
-            Data::Number(num) => format!("{}", num),
-            Data::Frame(argc, _) => format!("F{}", argc),
+            Data::Reference(reference) => reference.to_string(),
+            Data::Frame(argc, _, instr_pointer) => format!("F{},{}", argc, instr_pointer),
         }
     }
 }
@@ -68,25 +61,25 @@ pub struct Stack {
 impl Stack {
     pub fn new(capacity: usize) -> Stack {
         let mut stack =  Vec::<Data>::with_capacity(capacity);
-        stack.push(Data::Frame(0, 0));
+        stack.push(Data::Frame(0, 0, 0));
         Stack { stack, frame_index: 0 }
     }
 
     pub fn push(&mut self, data: Data) {
         match data {
-            Data::Number(_) => self.stack.push(data),
-            Data::Frame(args, _) => {
+            Data::Reference(_) => self.stack.push(data),
+            Data::Frame(args, _, instr_pointer) => {
                 while self.stack.len() - self.frame_index - 1 < args {
-                    self.push(Data::Number(0.0));
+                    self.push(Data::Reference(Reference::None));
                 }
                 self.frame_index = self.stack.len();
-                self.stack.push(Data::Frame(args, self.frame_index));
+                self.stack.push(Data::Frame(args, self.frame_index, instr_pointer));
             },
         }
     }
 
     pub fn pop(&mut self) -> Option<Data> {
-        if matches!(self.stack.last(), Some(Data::Frame(_, _))) {
+        if matches!(self.stack.last(), Some(Data::Frame(_, _, _))) {
             None
         } else {
             self.stack.pop()
@@ -94,15 +87,15 @@ impl Stack {
     }
 
     pub fn peek(&self) -> Option<Data> {
-        if matches!(self.stack.last(), Some(Data::Frame(_, _))) {
+        if matches!(self.stack.last(), Some(Data::Frame(_, _, _))) {
             None
         } else {
             self.stack.last().and_then(|last| Some(*last))
         }
     }
 
-    pub fn pop_frame(&mut self) {
-        while !self.stack.is_empty() && !matches!(self.stack.last(), Some(Data::Frame(_, _))) {
+    pub fn pop_frame(&mut self) -> Option<usize>{
+        while !self.stack.is_empty() && !matches!(self.stack.last(), Some(Data::Frame(_, _, _))) {
             self.stack.pop();
         }
 
@@ -110,35 +103,46 @@ impl Stack {
         if self.stack.len() > 1 {
             if let Some(last) = self.stack.pop() {
                 match last {
-                    Data::Number(_) => (),
-                    Data::Frame(argc, _) => {
+                    Data::Reference(_) => (),
+                    Data::Frame(argc, _, instr_pointer) => {
                         for _ in 0..argc {
                             self.stack.pop();
                         }
+
+                        return Some(instr_pointer)
                     },
                 }
             }
         }
+
+        None
     }
 
-    pub fn get_arg(&mut self, index: usize) -> Option<Data> {
+    pub fn get_arg(&mut self, index: usize) -> Option<Reference> {
         if index >= self.get_frame_argc() || index > self.frame_index || self.frame_index == 0 {
             None
         } else {
-            Some(self.stack[self.frame_index - 1 - index])
+            if let Data::Reference(reference) = self.stack[self.frame_index - 1 - index] {
+                Some(reference)
+            } else {
+                None
+            }
         }
     }
 
-    pub fn set_arg(&mut self, index: usize, number: f64) {
+    pub fn set_arg(&mut self, index: usize, reference: Reference) -> Option<Reference> {
         if !(index >= self.get_frame_argc() || index > self.frame_index || self.frame_index == 0) {
-            self.stack[self.frame_index - 1 - index] = Data::Number(number);
+            self.stack[self.frame_index - 1 - index] = Data::Reference(reference);
+            Some(reference)
+        } else {
+            None
         }
     }
 
     fn get_frame_argc(&self) -> usize {
         match self.stack[self.frame_index] {
-            Data::Number(_) => 0,
-            Data::Frame(argc, _) => argc,
+            Data::Reference(_) => 0,
+            Data::Frame(argc, _, _) => argc,
         }
     }
 }
@@ -164,17 +168,20 @@ impl Matrix {
         }
     }
 
-    pub fn set(&mut self, x:usize, y:usize, number:f64) {
+    pub fn set(&mut self, x:usize, y:usize, number:f64) -> Option<f64> {
         if x >= self.width || y >= self.height {
-            return
+            None
         } else {
             self.memory[y*self.width + x] = number;
+            Some(number)
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::vm::Reference;
+
     use super::{Matrix, Stack, Data};
 
     #[test]
@@ -183,7 +190,7 @@ mod tests {
 
         for y in 0..mat.height {
             for x in 0..mat.width {
-                mat.set(x, y, (x*y) as f64)
+                mat.set(x, y, (x*y) as f64);
             }
         }
 
@@ -204,11 +211,11 @@ mod tests {
         let mut stack = Stack::new(10);
 
         for i in 0..10 {
-            stack.push(Data::Number(i as f64));
+            stack.push(Data::Reference(Reference::Literal(i as f64)));
         }
 
         for i in (0..10).rev() {
-            assert_eq!(stack.pop().unwrap(), Data::Number(i as f64));
+            assert_eq!(stack.pop().unwrap(), Data::Reference(Reference::Literal(i as f64)));
         }
 
         assert_eq!(stack.pop(), None);
@@ -220,40 +227,40 @@ mod tests {
 
         assert_eq!(stack.get_frame_argc(), 0);
 
-        stack.push(Data::Number(1.0));
-        stack.push(Data::Number(2.0));
+        stack.push(Data::Reference(Reference::Literal(1.0)));
+        stack.push(Data::Reference(Reference::Literal(2.0)));
 
-        stack.push(Data::Frame(2, 0));
+        stack.push(Data::Frame(2, 0, 0));
 
         assert_eq!(stack.stack.len(), 4);
-        assert_eq!(stack.get_arg(0).unwrap(), Data::Number(2.0));
-        assert_eq!(stack.get_arg(1).unwrap(), Data::Number(1.0));
+        assert_eq!(stack.get_arg(0).unwrap(), Reference::Literal(2.0));
+        assert_eq!(stack.get_arg(1).unwrap(), Reference::Literal(1.0));
 
         assert_eq!(stack.pop(), None);
 
-        stack.push(Data::Number(-1.0));
+        stack.push(Data::Reference(Reference::Literal(-1.0)));
         
-        assert_eq!(stack.pop().unwrap(), Data::Number(-1.0));
+        assert_eq!(stack.pop().unwrap(), Data::Reference(Reference::Literal(-1.0)));
 
-        stack.push(Data::Number(3.0));
-        stack.push(Data::Frame(3, 0));
+        stack.push(Data::Reference(Reference::Literal(3.0)));
+        stack.push(Data::Frame(3, 0, 0));
 
         assert_eq!(stack.stack.len(), 8);
-        assert_eq!(stack.get_arg(0).unwrap(), Data::Number(0.0));
-        assert_eq!(stack.get_arg(1).unwrap(), Data::Number(0.0));
-        assert_eq!(stack.get_arg(2).unwrap(), Data::Number(3.0));
+        assert_eq!(stack.get_arg(0).unwrap(), Reference::Literal(0.0));
+        assert_eq!(stack.get_arg(1).unwrap(), Reference::Literal(0.0));
+        assert_eq!(stack.get_arg(2).unwrap(), Reference::Literal(3.0));
 
-        stack.push(Data::Number(6.0));
-        stack.push(Data::Number(5.0));
-        stack.push(Data::Number(4.0));
+        stack.push(Data::Reference(Reference::Literal(6.0)));
+        stack.push(Data::Reference(Reference::Literal(5.0)));
+        stack.push(Data::Reference(Reference::Literal(4.0)));
 
-        stack.push(Data::Frame(1, 0));
+        stack.push(Data::Frame(1, 0, 0));
 
-        assert_eq!(stack.get_arg(0).unwrap(), Data::Number(4.0));
+        assert_eq!(stack.get_arg(0).unwrap(), Reference::Literal(4.0));
 
         stack.pop_frame();
 
-        assert_eq!(stack.pop().unwrap(), Data::Number(5.0));
+        assert_eq!(stack.pop().unwrap(), Data::Reference(Reference::Literal(5.0)));
 
         stack.pop_frame();
 
